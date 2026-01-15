511c1c8b4b5f7ebd12b4f6a6e9db5249738612a3
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-15 21:43:16 +0530

    schema created

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express');
+const { Schema, default: mongoose } = require('mongoose');
 require('./db_connection')
 
 const app = express();
 
 const PORT = 5000;
 
-app.get('/node',(req,res)=>{
-try {
+// app.get('/node',(req,res)=>{
+// try {
 
-    res.status(200).json({
-        status:200,
-        message:"This is a get api",
-    });
+//     res.status(200).json({
+//         status:200,
+//         message:"This is a get api",
+//     });
     
-} catch (err) {
-    res.status(500).json({
-        message:"Server Error",
-    });
-}
+// } catch (err) {
+//     res.status(500).json({
+//         message:"Server Error",
+//     });
+// }
+// });
+
+
+const todoSchema = new mongoose.Schema({
+    title : String,
+    description : String
 });
+
+
+const todoModel = 
 
 app.listen(PORT,()=>{
     console.log(`Server is running at ${PORT}`)
