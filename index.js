14f3bd45ab2f336ae35f037e42c4304fbe621818
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-15 22:02:24 +0530

    get and delete api also created

--- index.js
+++ index.js
@@ -42,6 +42,26 @@ app.post('/create-todo',async (req,res)=>{
     }
 });
 
+app.get('/get-todo',async (req,res)=>{
+    try {
+        const todo = await todoModel.find(req.body);
+        res.status(200).json({message:"todo,s fetched",data:todo})
+        
+    } catch (err) {
+        res.status(500).json({message:"Server error"});
+    }
+});
+
+app.delete('/delete-todo',async (req,res)=>{
+    try {
+        const todo = await todoModel.delete(req.body);
+        res.status(200).json({message:"todo,s deleted",})
+        
+    } catch (err) {
+        res.status(500).json({message:"Server error"});
+    }
+});
+
 app.listen(PORT,()=>{
     console.log(`Server is running at ${PORT}`)
 })
