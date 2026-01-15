847b53382c51ea150726d376060235b99bad5c7c
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-15 21:50:49 +0530

    first post api created

--- index.js
+++ index.js
@@ -28,7 +28,17 @@ const todoSchema = new mongoose.Schema({
 });
 
 
-const todoModel = 
+const todoModel = mongoose.model('todoS',todoSchema);
+
+app.post('/create-todo',async (req,res)=>{
+    try {
+        const todo = await todoModel.create(req.body);
+        res.status(200).json({message:"new todo added",data:todo})
+        
+    } catch (err) {
+        res.status(500).json({message:"Server error"});
+    }
+});
 
 app.listen(PORT,()=>{
     console.log(`Server is running at ${PORT}`)
