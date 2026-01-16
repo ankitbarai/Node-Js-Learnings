cf949fa14a0981fb24af102f7e21655f0e2f7e75
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-16 21:15:41 +0530

    finally complete

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,24 +1,27 @@
 const userModel = require('../models/user_models')
 // user regisstration
 
-const userRegister = async (req,res)=>{
-    const {name,email,password} = req.body;
+const userRegister = async (req, res) => {
+    const { name, email, password } = req.body;
 
     try {
-        if (!(name&&email&&password)) {
-            return res.statuscode(400).send({success:false,message:"Name,email and password are required"});
+        if (!(name && email && password)) {
+            return res.statuscode(400).send({ success: false, message: "Name,email and password are required" });
         }
 
-        const getUser = await userModel.findOne({email:email});
+        const getUser = await userModel.findOne({ email: email });
 
         if (getUser) {
-            return res.statuscode(400).send({success:false,message:"Email already registered"});
-        }else{
-            return res.statuscode(200).send({success:false,message:"User registered Successfully"});
-        }   
+            return res.statuscode(400).send({ success: false, message: "Email already registered" });
+        }
+
+        let randomOTP = Math.floor(Math.random() * 1000) + 1000;
+        console.log("OTP is", randomOTP);
+        return res.statuscode(200).send({ success: true, message: "User registered Successfully",data:{yourOTP:randomOTP} });
+
     } catch (err) {
-        return res.statuscode(500).send({success:false,message:"Internal Server error"});
+        return res.statuscode(500).send({ success: false, message: "Internal Server error" });
     }
 }
 
-module.exports = {userRegister};
+module.exports = { userRegister };
